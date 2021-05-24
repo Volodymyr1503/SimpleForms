ba9e2a6a21b679fa9880df65dbe1e7a2d1629666
Volodymyr <volodymyr15.2017@gmail.com>
2021-05-24 14:35:27 +0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const Heroes: Hero [] = [
+  { id: 11, name: 'Dr Nice', date: "20.03.2020" },
+  { id: 12, name: 'Narco', date: "21.03.2021" },
+  { id: 13, name: 'Bombasto', date: "20.09.2020" },
+  { id: 14, name: 'Celeritas', date: "02.03.2019" },
+  { id: 15, name: 'Magneta', date: "20.05.2012" },
+  { id: 16, name: 'RubberMan', date: "20.08.2020" },
+  { id: 17, name: 'Dynama', date: "20.03.2019" },
+  { id: 18, name: 'Dr IQ', date: "20.03.2018" },
+  { id: 19, name: 'Magma', date: "20.03.2017" },
+  { id: 20, name: 'Tornado', date: "20.03.2016" }
+]
